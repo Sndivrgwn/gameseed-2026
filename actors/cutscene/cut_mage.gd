ea1dcc57e460e6e8fa004e01b45d100d3733c732
rafli1127 <mmuhammadrafli.tkj@gmail.com>
2026-07-02 20:24:41 +0700

    change cut script

--- actors/cutscene/cut_mage.gd
+++ actors/cutscene/cut_mage.gd
@@ -1,27 +1,52 @@
 extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-@onready var movement: MovementComponent = $MovementComponent
-@onready var animation: AnimationComponent = $AnimationComponent
-@export var speed = 250
 
-var can_move = true
+@export var speed := 250
+
+var can_move := true
+var last_direction := "down"
 
 func _physics_process(delta):
 
 	if !can_move:
 		anim.play("idle_down")
+		velocity = Vector2.ZERO
+		move_and_slide()
 		return
-	var direction = Vector2.ZERO
-	
-	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	
-	direction = direction.normalized()
-	movement.move(direction)
-	animation.handle_animation(direction)
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	body.can_move = false
+	var direction = Input.get_vector(
+		"move_left",	
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+
+	velocity = direction * speed
+	move_and_slide()
+
+	handle_animation(direction)
 	
-	
+func handle_animation(direction: Vector2):
+
+	if direction == Vector2.ZERO:
+		anim.play("idle_" + last_direction)
+		return
+
+	if abs(direction.x) > abs(direction.y):
+
+		if direction.x > 0:
+			last_direction = "right"
+			anim.play("right")
+		else:
+			last_direction = "left"
+			anim.play("left")
+
+	else:
+
+		if direction.y > 0:
+			last_direction = "down"
+			anim.play("down")
+		else:
+			last_direction = "up"
+			anim.play("up")
